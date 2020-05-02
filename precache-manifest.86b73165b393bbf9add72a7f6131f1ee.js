67c998b8e606c2a537735397d046e736062d4d13
Joshua Pravin Raj <orifielgamer@gmail.com>
2020-05-02 18:33:08 +0800

    Updates

---
+++ precache-manifest.86b73165b393bbf9add72a7f6131f1ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd749cf93fefa941f5917b9e8447a90",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "26d2e3472644c16a520c",
+    "url": "/monster-rolodex/static/css/main.2fbb61f1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d2e3472644c16a520c",
+    "url": "/monster-rolodex/static/js/main.54f6f565.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
